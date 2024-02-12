7e8e663e5160d654337f8fe80dbc32cfa2ed144f
Uriel Awe-Obe <urielaweobe@gmail.com>
2024-02-12 17:36:21 +0100

    setting up with tailwind css

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>CSU LAUTECH CHAPTER</h1>
+      <h1 className="text-3xl font-bold underline">CSU LAUTECH CHAPTER</h1>
     </div>
   );
 }
